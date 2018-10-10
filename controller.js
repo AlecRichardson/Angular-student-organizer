7cd3e53b444c383fe8cdda5272bbb9acd0684da2
Alec Richardson <alec.richardson@mail.missouri.edu>
2018-10-09 22:24:36 -0500

    Store added students in local storage, keep track of active and inactive students, remove from storage when deleted, switch view depending on whats selected

--- controller.js
+++ controller.js
@@ -1,19 +1,27 @@
 var app = angular.module("studentOrganizer", []);
 
 app.controller("myCtrl", function($scope) {
-  $scope.students = [
-    {
-      name: "Alec",
-      studentNumber: 14295160,
-      address: "123 Rollins st",
-      phoneNumber: 1234567890,
-      gpa: 4.0,
-      plan: "IT",
-      year: "Senior"
+  $scope.students = [];
+  $scope.studentsActive = [];
+  $scope.studentsInactive = [];
+  $scope.activetotal = 0;
+  $scope.inactivetotal = 0;
+  var retrievedStudents = 0;
+  if (localStorage.getItem("Students")) {
+    retrievedStudents = JSON.parse(localStorage.getItem("Students") || []);
+  }
+  $scope.init = function() {
+    for (var i = 0; i < retrievedStudents.length; i++) {
+      if (retrievedStudents[i].activity === "Active") {
+        $scope.activetotal++;
+        $scope.studentsActive.push(retrievedStudents[i]);
+      } else {
+        $scope.inactivetotal++;
+        $scope.studentsInactive.push(retrievedStudents[i]);
+      }
+      $scope.students.push(retrievedStudents[i]);
     }
-  ];
-
-  var array = JSON.parse(localStorage.getItem("Students") || []);
+  };
 
   $scope.addStudent = function() {
     $scope.errortext = "";
@@ -21,6 +29,13 @@ app.controller("myCtrl", function($scope) {
       return;
     }
     if ($scope.students.indexOf($scope.student) == -1) {
+      if ($scope.student.activity === true) {
+        $scope.student.activity = "Active";
+        $scope.activetotal++;
+      } else {
+        $scope.student.activity = "Inactive";
+        $scope.inactivetotal++;
+      }
       $scope.students.push($scope.student);
       localStorage.setItem("Students", JSON.stringify($scope.students));
     } else {
@@ -31,6 +46,28 @@ app.controller("myCtrl", function($scope) {
   };
   $scope.removeItem = function(x) {
     $scope.errortext = "";
+    if ($scope.students[x].activity === "Active") {
+      $scope.activetotal--;
+    } else {
+      $scope.inactivetotal--;
+    }
     $scope.students.splice(x, 1);
+  };
+
+  $scope.toggleActive = function(i, data) {
+    if ($scope.students[i].activity === data) {
+      return;
+    }
+    if (data === "Active") {
+      $scope.students[i].activity = "Active";
+      $scope.inactivetotal--;
+      $scope.activetotal++;
+      return;
+    } else {
+      $scope.students[i].activity = "Inactive";
+      $scope.inactivetotal++;
+      $scope.activetotal--;
+      return;
+    }
   };
 });
